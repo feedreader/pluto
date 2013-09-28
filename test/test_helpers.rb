bf1605198da902344e6bf4abd3491087706df801
Gerald Bauer <gerald.bauer@gmail.com>
2013-09-28 10:16:54 +0200

    add whitelist tests

--- test/test_helpers.rb
+++ test/test_helpers.rb
@@ -31,6 +31,24 @@ class TestHelper < MiniTest::Unit::TestCase
 <p>Details: http://www.meetup.com/vienna-rb/events/130346182/</p>
 EOS
 
+    hystep1 = <<EOS
+‹p›♦vienna.rb‹/p›
+‹p›
+  ‹p›The cool guys from Platogo will sponsor (y)our drinks. Which is awesome.‹/p›
+  ‹p›Talks*‹/p›
+  ‹p›Jakob Sommerhuber - sponsor talk‹/p›
+  ‹p›Martin Schürrer - Erlang/OTP in production for highly-available, scalable systems‹/p›
+  ‹p›Markus Prinz - How to improve your code‹/p›
+  ‹p›Gerald Bauer - working with Sinatra‹/p›
+  ‹p›Kathrin Folkendt - 'Chapter one' (lightning talk on getting started with Rails, and building her first web app)‹/p›
+  ‹p›*preliminary program‹/p›
+‹/p›
+‹p›Vienna   - Austria‹/p›
+‹p›Friday, October 11 at 6:00 PM‹/p›
+‹p›Attending: 21‹/p›
+‹p›Details: www.meetup.com/vienna-rb/events/130346182/‹/p›
+EOS
+
     hyout = <<EOS
 <p>♦vienna.rb</p>
 <p>
@@ -49,13 +67,9 @@ EOS
 <p>Details: www.meetup.com/vienna-rb/events/130346182/</p>
 EOS
 
-    out = textify( hyin )
-    
-    puts '==================>'
-    puts out
-    puts '<=================='
-    
-    assert( out == hyout)
+    assert_equal( hyout, textify( hyin ) )
+
+    assert_equal( hystep1, textify( hyin, skip_restore: true ) )
   end
 
 end # class TestHelper
