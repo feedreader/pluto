9573cb077a45c48d7333064824e0892432812b5c
Gerald Bauer <gerald.bauer@gmail.com>
2014-12-26 14:54:39 +0100

    up generator

--- lib/pluto/version.rb
+++ lib/pluto/version.rb
@@ -4,7 +4,7 @@ module Pluto
 
   MAJOR = 1
   MINOR = 3
-  PATCH = 1
+  PATCH = 2
   VERSION = [MAJOR,MINOR,PATCH].join('.')
 
   def self.version
@@ -15,6 +15,13 @@ module Pluto
     ### todo: add RUBY_PATCHLEVEL or RUBY_PATCH_LEVEL  e.g. -p124
     "pluto-models/#{VERSION} on Ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE}) [#{RUBY_PLATFORM}]"
   end
+
+  ## Note: moved from pluto-merge (add here because pluto-merge gem is optional)
+  ##  fix: remove generator in pluto-merge!!! (duplicate)
+  def self.generator   # convenience alias for banner (matches HTML generator meta tag)
+    "Pluto #{VERSION} on Ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE}) [#{RUBY_PLATFORM}]"
+  end
+
 
   def self.root
     "#{File.expand_path( File.dirname(File.dirname(File.dirname(__FILE__))) )}"
