6c302fa12fb378bc21a975fd1fa05d5d1a358238
Gerald Bauer <gerald.bauer@gmail.com>
2020-02-12 17:16:53 +0100

    Update test_loop.rb

--- html-template/test/test_loop.rb
+++ html-template/test/test_loop.rb
@@ -1,6 +1,57 @@
 
+def test_loop_meta
+  [].each_with_loop do |item, item_loop|
+    assert false   ## empty loop; fail/error if we get here
+  end
 
-def test_inner_example
+  [0].each_with_loop do |item, item_loop|
+    if item == 0
+     assert_equal 0, item_loop.index
+     assert_equal 1, item_loop.counter
+     assert          item_loop.odd?   == true
+     assert          item_loop.even?  == false
+     assert          item_loop.first? == true
+     assert          item_loop.inner? == false
+     assert          item_loop.outer? == true
+     assert          item_loop.last?  == true
+    end
+  end
+
+  [0,1,2].each_with_loop do |item, item_loop|
+     if item == 0
+      assert_equal 0, item_loop.index
+      assert_equal 1, item_loop.counter
+      assert          item_loop.odd?   == true
+      assert          item_loop.even?  == false
+      assert          item_loop.first? == true
+      assert          item_loop.inner? == false
+      assert          item_loop.outer? == true
+      assert          item_loop.last?  == false
+     end
+     if item == 1
+      assert_equal 1, item_loop.index
+      assert_equal 2, item_loop.counter
+      assert          item_loop.odd?   == false
+      assert          item_loop.even?  == true
+      assert          item_loop.first? == false
+      assert          item_loop.inner? == true
+      assert          item_loop.outer? == false
+      assert          item_loop.last?  == false
+     end
+     if item == 2
+      assert_equal 2, item_loop.index
+      assert_equal 3, item_loop.counter
+      assert          item_loop.odd?   == true
+      assert          item_loop.even?  == false
+      assert          item_loop.first? == false
+      assert          item_loop.inner? == false
+      assert          item_loop.outer? == true
+      assert          item_loop.last?  == true
+     end
+  end
+end
+
+def test_loop_inner
   t = HtmlTemplate.new( <<TXT )
 <TMPL_LOOP feeds>
   <TMPL_VAR title>
