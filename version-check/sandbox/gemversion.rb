15147c2ca64c5eb3b49e52fdd3cb0add2b56d7bf
Gerald Bauer <gerald.bauer@gmail.com>
2020-02-25 17:02:50 +0100

    Create gemversion.rb

---
+++ version-check/sandbox/gemversion.rb
@@ -0,0 +1,22 @@
+###
+## see https://ruby-doc.org/stdlib-2.5.0/libdoc/rubygems/rdoc/Gem/Version.html
+##     https://docs.ruby-lang.org/en/2.6.0/Gem/Version.html
+
+require 'pp'
+
+versions_a = ['1.0',
+              '1.0.b1',
+              '1.0.1',
+              '1.0.b.2',
+              '1.0.b10',
+              '1.0.a.2',
+              '0.9',
+              '1.0.a10'
+             ]
+
+versions_a = versions_a.map { |v| Gem::Version.create( v ) }
+
+pp versions_a
+pp versions_a.sort
+
+pp version_a = versions_a.sort.map { |v| v.to_s }
