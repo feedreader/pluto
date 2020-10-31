5105cef814c52ac9da9d2323da4e3e88fac3223c
Gerald Bauer <gerald.bauer@gmail.com>
2020-10-31 17:55:44 +0100

    up date-formatter

--- date-formatter/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-
-### 0.0.1 / 2020-02-16
-
-* Everything is new. First release
--- date-formatter/lib/date/formatter.rb
+++
@@ -1,1 +0,0 @@
-require_relative '../date-formatter'     ##  note: allow require 'date/formatter' too (in addition to require 'date-formatter')
